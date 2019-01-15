1e8db05da55c22eca332683ce55b31591901322e
Vinnie <vinnie-@live.com>
2019-01-15 16:33:15 -0600

    Added number guessing game.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,34 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random rng = new Random();
+            Console.WriteLine("I'm thinking of a number between 0-100!");
+            int myNum = rng.Next(0, 101);
+            int userGuess = -1;
+            int numTries = 0;
+
+            while (userGuess != myNum)
+            {
+                Console.Write("Please enter your guess: ");
+                userGuess = Convert.ToInt32(Console.ReadLine());
+                numTries++;
+
+                if(userGuess < myNum)
+                {
+                    Console.WriteLine("Too low! Try again.");
+                }
+                else if(userGuess > myNum)
+                {
+                    Console.WriteLine("Too high! Try again.");
+                }
+                else
+                {
+                    Console.WriteLine("You won in {0} tries! Press any key to exit.", numTries);
+                    Console.ReadKey();
+                    return;
+                }
+            }
+            
         }
     }
 }
