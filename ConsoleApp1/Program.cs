f9a9bf56f28b5a2d2ebb492a47965c80f251c0ef
Vinnie <vinnie-@live.com>
2019-01-15 16:39:52 -0600

    Added lower/upper bound selection.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,8 +11,14 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Random rng = new Random();
-            Console.WriteLine("I'm thinking of a number between 0-100!");
-            int myNum = rng.Next(0, 101);
+
+            Console.Write("Choose a lower bound: ");
+            int lower = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Choose an upper bound: ");
+            int upper = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("I'm thinking of a number between {0}-{1}!", lower, upper);
+            int myNum = rng.Next(lower, upper+1);
             int userGuess = -1;
             int numTries = 0;
 
